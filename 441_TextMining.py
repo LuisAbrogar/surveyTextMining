d7369ab54afa2eed4ef274a19caf6cea516cdc12
Darren Li <lidarren@yahoo.com>
2021-03-22 07:39:19 -0400

    3/22/21 7:39AM - Darren Li

--- 441_TextMining.py
+++ 441_TextMining.py
@@ -2,6 +2,9 @@ import csv
 import requests
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 key="(insert subscription key)"
 endpoint="https://(insert subscription name).cognitiveservices.azure.com/"
@@ -21,32 +24,33 @@ def sentiment_analysis(text):
     sentiment.append(response.sentiment) #append overall sentiments into a list
 
 
-#def key_phrase_extraction(text):
-#    try:
-#        response2=client.extract_key_phrases(documents=text)[0]
+def key_phrase_extraction(text):
+    try:
+        response2=client.extract_key_phrases(documents=text)[0]
 
-#        if not response2.is_error:
-#            print("\tKey Phrases:\n")
-#            for phrase in response2.key_phrases:
-#                print("\t\t",phrase)
-#            print('\n')
+        if not response2.is_error:
+            for phrase in response2.key_phrases:
+                print(phrase)
 
-#        else:
-#            print(response2.id,response2.error,'\n')
+        else:
+            print(response2.id,response2.error)
 
-#    except Exception as err:
-#        print("Encountered exception. {}".format(err),'\n')
+    except exception as err:
+        print("encountered exception. {}".format(err))
 
 
-with open(r'C:\Users\(User)\(Folder)\community-survey-open-ended-comments-2016-2017-1.csv') as database:
+with open(r'c:\users\darren li\desktop\community-survey-open-ended-comments-2016-2017-1.csv') as database:
     data = csv.reader(database) 
     next(data) #skips the header
 
     count=0
     district_sentiment = {}
 
+    print("Key Phrases:")
+
     for row in data:
         msg=tuple([row[2]])         #converts the parsed strings into tuples
+        key_phrase_extraction(msg)
         sentiment_analysis(msg)     #textanalyticsclient functions only use tuples as arguments
         count+=1
 
@@ -55,17 +59,61 @@ with open(r'C:\Users\(User)\(Folder)\community-survey-open-ended-comments-2016-2
         else:
             district_sentiment[row[1]] = [sentiment[count-1]]
 
-        if count==10: #adjusts sample size
+        if count==30: #adjusts sample size
             break
 
 #print(district_sentiment)
 
-#this displays the percent sentiment per district
+#for x, y in district_sentiment.items():
+#    total = y.count("negative") + y.count("mixed") + y.count("positive") + y.count("neutral")
+#    print("District#" + str(x))
+#    print("Percent Negative: " + str( 100 * y.count("negative") / total )  + "%")
+#    print("Percent Mixed: " + str( 100 * y.count("mixed") / total )  + "%")
+#    print("Percent Positive: " + str( 100 * y.count("positive") / total )  + "%")
+#    print("Percent Neutral: " + str( 100 * y.count("neutral") / total )  + "%")
+#    print('\n')
+
+#making grouped bar graph for sentiment analysis
+labels = []
+positive = []
+negative = []
+mixed = []
+neutral = []
+
 for x, y in district_sentiment.items():
     total = y.count("negative") + y.count("mixed") + y.count("positive") + y.count("neutral")
-    print("District#" + str(x))
-    print("Percent Negative: " + str( 100 * y.count("negative") / total )  + "%")
-    print("Percent Mixed: " + str( 100 * y.count("mixed") / total )  + "%")
-    print("Percent Positive: " + str( 100 * y.count("positive") / total )  + "%")
-    print("Percent Neutral: " + str( 100 * y.count("neutral") / total )  + "%")
-    print('\n')
+    labels.append(x)
+    positive.append(100 * y.count("positive") / total)
+    negative.append(100 * y.count("negative") / total)
+    mixed.append(100 * y.count("mixed") / total)
+    neutral.append(100 * y.count("neutral") / total)
+
+x = np.arange(len(labels))
+width = 0.2
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3*width/2, positive, width, label='Positive')
+rects2 = ax.bar(x - width/2, negative, width, label='Negative')
+rects3 = ax.bar(x + width/2, mixed, width, label='Mixed')
+rects4 = ax.bar(x + 3*width/2, neutral, width, label='Neutral')
+
+ax.set_ylabel('Percent %')
+ax.set_xlabel('District #')
+ax.set_title('Sentiment Analysis of Community Survey')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+#def autolabel(rects):
+#    for rect in rects:
+#        height = rect.get_height()
+#        ax.annotate('{}'.format(height), xy=(rect.get_x() + rect.get_width()/2, height), xytext=(0,3), textcoords="offset points", ha='center', va='bottom')
+
+#autolabel(rects1)
+#autolabel(rects2)
+#autolabel(rects3)
+#autolabel(rects4)
+
+#fig.tight_layout()
+
+plt.show()
